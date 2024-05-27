d6b88bd16b3ec82ff3895215ffbefe5e923edd88
Fabricio-Lima <fabriciolc61@gmail.com>
2024-05-26 23:12:25 -0300

    Adiciona model do home e realiza chamada no inicio da aplicação

--- controllers/home_controller.py
+++ controllers/home_controller.py
@@ -1,35 +1,26 @@
 import PySimpleGUI as sG
 
 
+class HomeController:
+    def __init__(self):
+        self.layout = [
+            [sG.Button('Alunos')],
+            [sG.Button('Professores')],
+            [sG.Button('Disciplinas')],
+            [sG.Button('Turmas')],
+            [sG.Button('Salas de Aulas')],
+            [sG.Button('Horários')],
+            [sG.Button('Aulas')]
+        ]
+        self.window = None
 
-layout = [
-    [sG.Text("Consultar se um aluno está cursando uma disciplina:")],
-    [sG.Text("Aluno:"), sG.InputText(key="aluno")],
-    [sG.Text("Disciplina:"), sG.InputText(key="disciplina")],
-    [sG.Button("Consultar")],
-    [sG.Text("", size=(30, 1), key="consulta_resultado")],
-    [sG.Text("Calcular média de um aluno:")],
-    [sG.Text("Aluno:"), sG.InputText(key="aluno_media")],
-    [sG.Button("Calcular")],
-    [sG.Text("", size=(30, 1), key="calculo_resultado")],
-]
+    def retorno(self):
+        self.window = sG.Window('Sistema Acadêmico', self.layout)
 
-window = sG.Window("Consulta Prolog", layout)
+        while True:
+            event, values = self.window.read()
 
-while True:
-    event, values = window.read()
-    if event == sG.WINDOW_CLOSED:
-        break
-    elif event == "Consultar":
-        aluno_consulta = values["aluno"]
-        disciplina_consulta = values["disciplina"]
-        if list(prolog.query(f"cursando({aluno_consulta}, {disciplina_consulta})")):
-            window["consulta_resultado"].update(f"{aluno_consulta} está cursando {disciplina_consulta}.")
-        else:
-            window["consulta_resultado"].update(f"{aluno_consulta} não está cursando {disciplina_consulta}.")
-    elif event == "Calcular":
-        aluno_calculo = values["aluno_media"]
-        media = list(prolog.query(f"calcular_media({aluno_calculo}, Media)"))[0]["Media"]
-        window["calculo_resultado"].update(f"A média de {aluno_calculo} é {media:.2f}.")
+            if event == self.window.is_closed():
+                break
 
-window.close()
+        self.window.close()
