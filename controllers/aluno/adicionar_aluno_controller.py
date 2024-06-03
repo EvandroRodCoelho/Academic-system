435e62843411b8d578f272bfdb075409de64e376
Fabricio-Lima <fabriciolc61@gmail.com>
2024-06-03 00:37:13 -0300

    adiciona regra prolog para verificar de tamanho de campo

--- controllers/aluno/adicionar_aluno_controller.py
+++ controllers/aluno/adicionar_aluno_controller.py
@@ -1,36 +1,45 @@
+from pyswip import Prolog
+
 from service.page_service import NavegacaoService
 from views.aluno.adicionar_aluno import TelaAdicionarAluno
-#from model.queries_prolog import prolog;
 from model.aluno_model import AlunoModel
-import PySimpleGUI as sg
+import PySimpleGUI as sG
+
+
 class AdicionarAlunoController:
     def __init__(self):
         self.window = None
-        self.alunoModel = AlunoModel();
+        self.alunoModel = AlunoModel()
+        self.prolog = Prolog()
+
+        self.prolog.assertz("tamanho_campo_valido(X) :- string_length(X, Length), Length =< 100")
+
     def mostrar_tela(self):
         self.window = TelaAdicionarAluno().window
         self.retorno()
-    
-    def retorno(self):
-     while True:
-        event, values = self.window.read()
-        
-        if event == 'Cadastrar':
-            nome = values['nome']
-            endereco = values['endereco']
-            if nome and endereco:
-                 if len(nome) > 100 or len(endereco) > 100:
-                  sg.popup('Os campos tem uma tamanho máximo de 100 caracteres')
-                 else:
-                    self.alunoModel.adicionar_aluno(nome,endereco)
-                  #  prolog.assertz(f"aluno('{nome}')")
-                    sg.popup('Cadastro realizado com sucesso!', f'Nome: {nome}\nEndereço: {endereco}')
-            else:
-                sg.popup('Por favor, preencha todos os campos.')
-        
-        if event == sg.WIN_CLOSED or event == 'cancelar':
-            self.window.close();
-            paginaService = NavegacaoService()
-            paginaService.navegar_para_alunos()
-            break
 
+    def retorno(self):
+        while True:
+            event, values = self.window.read()
+
+            if event == 'Cadastrar':
+                nome = values['nome']
+                endereco = values['endereco']
+
+                if nome and endereco:
+                    tamanho_nome_valido = list(self.prolog.query(f"tamanho_campo_valido({nome})"))
+                    tamanho_endereco_valido = list(self.prolog.query(f"tamanho_campo_valido({endereco})"))
+
+                    if not tamanho_nome_valido or not tamanho_endereco_valido:
+                        sG.popup('Os campos tem uma tamanho máximo de 100 caracteres')
+                    else:
+                        self.alunoModel.adicionar_aluno(nome, endereco)
+                        sG.popup('Cadastro realizado com sucesso!', f'Nome: {nome}\nEndereço: {endereco}')
+                else:
+                    sG.popup('Por favor, preencha todos os campos.')
+
+            if event == sG.WIN_CLOSED or event == 'cancelar':
+                self.window.close()
+                paginaService = NavegacaoService()
+                paginaService.navegar_para_alunos()
+                break
--- controllers/aluno/editar_aluno_controller.py
+++ controllers/aluno/editar_aluno_controller.py
@@ -1,7 +1,11 @@
+from pyswip import Prolog
+
 from model.aluno_model import AlunoModel
 from service.page_service import NavegacaoService
-from views.aluno.editar_aluno  import TelaEditarAluno
-import PySimpleGUI as sg
+from views.aluno.editar_aluno import TelaEditarAluno
+import PySimpleGUI as sG
+
+
 class EditarAlunoController:
 
     def __init__(self, aluno):
@@ -9,29 +13,36 @@ class EditarAlunoController:
         self.navegacaoService = NavegacaoService()
         self.aluno = aluno
         self.alunoModel = AlunoModel()
+        self.prolog = Prolog()
+
+        self.prolog.assertz("tamanho_campo_valido(X) :- string_length(X, Length), Length =< 100")
+
     def mostrar_tela(self):
         self.window = TelaEditarAluno(self.aluno).window
         self.retorno()
-    
+
     def retorno(self):
-     while True:
-        event, values = self.window.read()
-        
-        if event == 'Salvar':
-            nome = values['nome']
-            endereco = values['endereco']
-            if nome and endereco:
-                 if len(nome) > 100 or len(endereco) > 100:
-                  sg.popup('Os campos tem uma tamanho máximo de 100 caracteres')
-                 else:
-                    self.alunoModel.atualizar_aluno(nome,endereco,self.aluno['id'])
-                    self.navegacaoService.navegar_para_alunos();
+        while True:
+            event, values = self.window.read()
 
-            else:
-                sg.popup('Por favor, preencha todos os campos.')
-        
-        if event == sg.WIN_CLOSED or event == 'cancelar':
-            self.window.close();
-            self.navegacaoService.navegar_para_alunos()
-            break
+            if event == 'Salvar':
+                nome = values['nome']
+                endereco = values['endereco']
 
+                if nome and endereco:
+                    tamanho_nome_valido = list(self.prolog.query(f"tamanho_campo_valido({nome})"))
+                    tamanho_endereco_valido = list(self.prolog.query(f"tamanho_campo_valido({endereco})"))
+
+                    if not tamanho_nome_valido or not tamanho_endereco_valido:
+                        sG.popup('Os campos tem uma tamanho máximo de 100 caracteres')
+                    else:
+                        self.alunoModel.atualizar_aluno(nome, endereco, self.aluno['id'])
+                        self.navegacaoService.navegar_para_alunos()
+
+                else:
+                    sG.popup('Por favor, preencha todos os campos.')
+
+            if event == sG.WIN_CLOSED or event == 'cancelar':
+                self.window.close()
+                self.navegacaoService.navegar_para_alunos()
+                break
