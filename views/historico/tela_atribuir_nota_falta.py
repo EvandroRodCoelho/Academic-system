2f7ab1c12b3ec696fbd2a7bbb37f07f9438be406
Fabricio-Lima <fabriciolc61@gmail.com>
2024-06-18 02:27:40 -0300

    Correções de sintaxe (pycharm)

--- views/historico/tela_atribuir_nota_falta.py
+++ views/historico/tela_atribuir_nota_falta.py
@@ -1,12 +1,16 @@
 import PySimpleGUI as sg
 
+
 class TelaAtribuirNotaFalta:
     def __init__(self):
         self.layout = [
-            [sg.Text('Digite a nota:', font=("Helvetica", 14)),sg.Push() ,sg.InputText(key='nota',size=(10, 1) ,font=("Helvetica", 14))],
-            [sg.Text('Digite a quantidade de faltas:' ,font=("Helvetica", 14)), sg.InputText(key='faltas',size=(10, 1) ,font=("Helvetica", 14))],
-            [sg.Button('Confirmar', size=(10, 1),font=("Helvetica", 14), key=('confirmar')),
-              sg.Button('Cancelar',size=(10, 1), font=("Helvetica", 14))]
+            [sg.Text('Digite a nota:', font=("Helvetica", 14)),
+             sg.Push(),
+             sg.InputText(key='nota', size=(10, 1), font=("Helvetica", 14))],
+            [sg.Text('Digite a quantidade de faltas:',
+                     font=("Helvetica", 14)), sg.InputText(key='faltas', size=(10, 1), font=("Helvetica", 14))],
+            [sg.Button('Confirmar', size=(10, 1), font=("Helvetica", 14), key=('confirmar')),
+             sg.Button('Cancelar', size=(10, 1), font=("Helvetica", 14))]
         ]
         self.window = sg.Window('Atribuir Nota e Faltas', self.layout)
 
