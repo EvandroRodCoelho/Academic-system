2a8cee0cc2d9be125810161713d602f74c00298a
Fabricio-Lima <fabriciolc61@gmail.com>
2024-06-18 16:40:18 -0300

    Corrige preenchimento do campo de sala de aula

--- views/alunos_sala_de_aula/adicionar_alunos_sala_de_aula.py
+++ views/alunos_sala_de_aula/adicionar_alunos_sala_de_aula.py
@@ -2,15 +2,18 @@ import PySimpleGUI as sg
 
 
 class AdicionarAlunosSalaDeAula:
-    def __init__(self, alunos, sala_de_aula):
+    def __init__(self, alunos, professores, disciplinas, salas_de_aulas):
         self.aluno_nome = [aluno[1] for aluno in alunos]
-        self.sala_de_aula = [sala[1] for sala in sala_de_aula]
+        self.salas_de_aulas = [sala[1] for sala in salas_de_aulas]
+
+        self.professores_nome = [prof[1] for prof in professores]
+        self.disciplinas_nome = [disc[1] for disc in disciplinas]
 
         self.layout = [
             [sg.Text('Aluno:', font=("Helvetica", 16))],
             [sg.Combo(self.aluno_nome, key='aluno', readonly=True, size=(30, 1), font=("Helvetica", 14))],
             [sg.Text('Sala de Aula:', font=("Helvetica", 16))],
-            [sg.Combo(self.sala_de_aula, key='sala_de_aula', readonly=True, size=(30, 1), font=("Helvetica", 14))],
+            [sg.Combo(self.salas_de_aulas, key='sala_de_aula', readonly=True, size=(30, 1), font=("Helvetica", 14))],
             [sg.Button('Cadastrar', font=("Helvetica", 16)),
              sg.Button('Cancelar', font=("Helvetica", 16), key='cancelar')]
         ]
