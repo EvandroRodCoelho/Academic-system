6a1d6675962e0a0d603db9f06684aeca212ab0d7
Fabricio-Lima <fabriciolc61@gmail.com>
2024-06-19 00:00:44 -0300

    Adiciona metodo para adicionar historico de aluno

--- model/historico_aluno_model.py
+++ model/historico_aluno_model.py
@@ -1,6 +1,7 @@
 import sqlite3
 from database.conexao import Conexao
 
+
 class HistoricoAlunoModel:
     def __init__(self):
         self.db = Conexao()
@@ -20,15 +21,15 @@ class HistoricoAlunoModel:
         finally:
             self.db.fechar_conn()
 
-    def obter_dados_historico(self, aluno_id, id_sala):
+    def obter_dados_historico(self, aluno_id, id_disciplina):
         try:
             self.db.iniciar_conn()
             query = '''
                 SELECT notas, faltas 
                 FROM historico_alunos
-                WHERE id_aluno = ? AND id_sala = ?
+                WHERE id_aluno = ? AND id_disciplina = ?
             '''
-            self.db.executar_sql(query, (aluno_id, id_sala))
+            self.db.executar_sql(query, (aluno_id, id_disciplina))
             return self.db.fetchall()
         except sqlite3.Error as e:
             print(f"Erro ao buscar dados do histórico: {e}")
@@ -59,24 +60,25 @@ class HistoricoAlunoModel:
                 WHERE id_aluno = ? AND id_disciplina = ?
             '''
             self.db.executar_sql(query, (nota, id_aluno, disciplina_id,))
-          
+
         except Exception as e:
             print(f"Erro ao atribuir nota: {str(e)}")
         finally:
             self.db.fechar_conn()
-    
-    def atribuir_falta_especifico(self, id_aluno, id_disciplina, faltas):     
+
+    def atribuir_falta_especifico(self, id_aluno, id_disciplina, faltas):
         try:
             self.db.iniciar_conn()
             query = '''
             UPDATE historico_alunos 
             SET faltas =  ? 
-            WHERE id_aluno = ? AND id_disciplina = ?'''        
+            WHERE id_aluno = ? AND id_disciplina = ?'''
             self.db.executar_sql(query, (faltas, id_aluno, id_disciplina))
         except Exception as e:
             print(f"Erro ao atribuir falta: {str(e)}")
         finally:
             self.db.fechar_conn()
+
     def atribuir_nota_e_falta(self, id_aluno, id_disciplina, nota, faltas):
         print(f"id_aluno:{id_aluno} id_disciplina:{id_disciplina} nota:{nota} faltas:{faltas} ")
         try:
@@ -90,5 +92,18 @@ class HistoricoAlunoModel:
             self.db.executar_sql(query, (nota, faltas, id_aluno, id_disciplina,))
         except Exception as e:
             print(f"Erro ao atribuir nota e faltas: {str(e)}")
+        finally:
+            self.db.fechar_conn()
+
+    def adicionar_historico_alunos(self, id_aluno, id_disciplina, notas, faltas):
+        try:
+            self.db.iniciar_conn()
+            query = '''
+                INSERT INTO historico_alunos (id_aluno, id_disciplina, notas, faltas)
+                VALUES (?, ?, ?, ?)
+            '''
+            self.db.executar_sql(query, (id_aluno, id_disciplina, notas, faltas))
+        except sqlite3.Error as e:
+            print(f"Erro ao adicionar histórico: {e}")
         finally:
             self.db.fechar_conn()
